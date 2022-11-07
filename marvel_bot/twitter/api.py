76d41c9a1a083115ec4b314fb3b534fb14d24e04
Ariel Parra <darkaico@gmail.com>
2022-11-07 20:10:47 +0100

    chore: refactoring twitter api wrapper

--- marvel_bot/twitter/api.py
+++ marvel_bot/twitter/api.py
@@ -4,16 +4,25 @@ import tweepy
 
 from marvel_bot.utils.singleton import SingletonMixin
 
+API_KEY = os.getenv("TW_API_KEY")
+SECRET_KEY = os.getenv("TW_SECRET_KEY")
+ACCESS_TOKEN = os.getenv("TW_ACCESS_TOKEN")
+ACCESS_SECRET_TOKEN = os.getenv("TW_ACCESS_TOKEN_SECRET")
+
 
 class TwitterAPI(SingletonMixin):
-    API_KEY = os.getenv("TW_API_KEY")
-    SECRET_KEY = os.getenv("TW_SECRET_KEY")
-    ACCESS_TOKEN = os.getenv("TW_ACCESS_TOKEN")
-    ACCESS_SECRET_TOKEN = os.getenv("TW_ACCESS_TOKEN_SECRET")
-
-    def __init__(self):
-        auth = tweepy.OAuthHandler(self.API_KEY, self.SECRET_KEY)
-        auth.set_access_token(self.ACCESS_TOKEN, self.ACCESS_SECRET_TOKEN)
+    def __init__(
+        self,
+        api_key: str = None,
+        api_secret_key: str = None,
+        access_token: str = None,
+        access_secret_token: str = None,
+    ):
+        auth = tweepy.OAuthHandler(api_key or API_KEY, api_secret_key or SECRET_KEY)
+        auth.set_access_token(
+            access_token or ACCESS_TOKEN,
+            access_secret_token or ACCESS_SECRET_TOKEN,
+        )
 
         self.api = tweepy.API(auth)
 
